f882cf1414ddc9d80b9f9c01ed2199a943f39280
DevMasterSh <dev_master_sh@mail.ru>
2023-01-05 23:35:54 +0300

    Solution final Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArrayStrings()
+{
+    string[] array;
+    Console.WriteLine("Введите массив строк через пробел:");
+    string m =Console.ReadLine();
+    array = m.Split(' ');
+    return array;
+}
+void CreateNewArrayStrings()
+{
+    string[] firstArray = InputArrayStrings();    
+    string str = string.Join(", ", firstArray);
+    Console.Write($"[{str}] ->");
+    int count = 0;
+    string[] secondArray = new string[firstArray.Length];
+    foreach (var i in firstArray)
+    {
+        if (i.Length <= 3)
+        {
+            secondArray[count] = i;
+            count++;            
+        }        
+    }
+    secondArray = secondArray
+       .Where(x => x != null)
+       .ToArray();                              // Удаляем пустые строки из итогового массива secondArray и перезаписываем его
+    str = string.Join(", ", secondArray);
+    Console.WriteLine($"[{str}]"); 
+}
+CreateNewArrayStrings();     
